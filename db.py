efb25e79257cf80f3a273a866c21292cb46411ad
Lev <levenikeev0@gmail.com>
2022-06-25 10:53:56 +0300

    добавили функцию по написанию

--- db.py
+++ db.py
@@ -1,4 +1,15 @@
 import sqlite3  # модуль sqlite
 conn = sqlite3.connect("Discord.db")  # или :memory:
 cursor = conn.cursor()
-cost = conn.row[2]
+
+def addmoney(money, date):
+    cursor.execute(f"""INSERT INTO money (money, date)
+                    VALUES ({money}, '{date}')""")
+    conn.commit()
+
+
+def balance():
+    cursor.execute(f"""SELECT sum(money) FROM money""")
+    data = cursor.fetchone()
+    return data[0]
+print(balance())
