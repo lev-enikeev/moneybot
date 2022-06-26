31348a2160df772d807eb54613747dbbc34786df
Lev <levenikeev0@gmail.com>
2022-06-26 15:18:01 +0300

    добавил нормальные команды на balance и pay

--- moneybot.py
+++ moneybot.py
@@ -1,7 +1,7 @@
 import json  # используется только для обработки инвентаря, но ему можно найти и другое применение
 import requests
 import discord
-
+import db
 from credentials import TOKEN
 from tabulate import tabulate  # удобный модуль для рисования таблиц
 from discord.ext import commands
@@ -15,7 +15,8 @@ moneys = 0
 
 
 @bot.command()
-async def pay(ctx, money):
+async def pay(ctx, money, date):
+    db.addmoney(money, date)
     await ctx.send("вы получили + " + money)
     global moneys
     moneys = moneys + int(money)
@@ -25,5 +26,5 @@ async def pay(ctx, money):
 async def balance(ctx):
     global moneys
     cost = moneys
-    await ctx.send(f"У вас {moneys}")
+    await ctx.send(f"У вас {db.balance()}")
 bot.run(TOKEN)
